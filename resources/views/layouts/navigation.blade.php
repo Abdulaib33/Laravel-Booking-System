f4bea63b3e554abb37b30acc8b69a36abc163c4e
Abdoulaye <Abdulaibayo@outlook.fr>
2026-03-20 05:25:37 +0100

    Improved Navigation links

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -12,14 +12,20 @@
 
                 <!-- Navigation Links -->
                 <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
-                    <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
+                    {{-- <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
                         {{ __('Dashboard') }}
-                    </x-nav-link>
+                    </x-nav-link> --}}
+
                     <x-nav-link :href="route('bookings.create')" :active="request()->routeIs('bookings.create')">
                         {{ __('Book') }}
                     </x-nav-link>
+
                     <x-nav-link :href="route('admin.services.index')" :active="request()->routeIs('admin.services.index')">
                         {{ __('Bookings') }}
+                    </x-nav-link>
+
+                    <x-nav-link :href="route('admin.services.index')" :active="request()->routeIs('admin.services.index')">
+                        {{ __('Services') }}
                     </x-nav-link>
                 </div>
             </div>
